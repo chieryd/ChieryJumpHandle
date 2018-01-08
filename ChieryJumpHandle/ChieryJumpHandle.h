2d710d0f62cfd717e667d83758d3e1cf6eba2da4
handong.wang <handong.wang@qunar.com>
2018-01-08 17:06:55 +0800

    Initial Commit

---
+++ ChieryJumpHandle/ChieryJumpHandle.h
@@ -0,0 +1,13 @@
+//
+//  ChieryJumpHandle.h
+//  ChieryJumpHandle
+//
+//  Created by chiery on 2018/1/8.
+//  Copyright © 2018年 com.chiery.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ChieryJumpHandle : NSObject
+
+@end
---
+++ ChieryJumpHandle/ChieryJumpHandle.m
@@ -0,0 +1,13 @@
+//
+//  ChieryJumpHandle.m
+//  ChieryJumpHandle
+//
+//  Created by chiery on 2018/1/8.
+//  Copyright © 2018年 com.chiery.com. All rights reserved.
+//
+
+#import "ChieryJumpHandle.h"
+
+@implementation ChieryJumpHandle
+
+@end
