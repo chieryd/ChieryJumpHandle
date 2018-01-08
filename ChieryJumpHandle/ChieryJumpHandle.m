2663ef69e1f7591c5099bc0a499068ca6bc0ac8a
handong.wang <handong.wang@qunar.com>
2018-01-08 18:27:02 +0800

    增加jumpHandle demo

--- ChieryJumpHandle/ChieryJumpHandle.m
+++ ChieryJumpHandle/ChieryJumpHandle.m
@@ -7,7 +7,74 @@
 //
 
 #import "ChieryJumpHandle.h"
+#import "ChieryJumpHandlePrt.h"
 
 @implementation ChieryJumpHandle
+
++ (BOOL)jumpHandleOpenURL:(NSURL *)url {
+    return [self jumpHandleOpenURL:url
+                       withUrlData:nil
+                      responseDelg:nil
+                          userInfo:nil];
+}
+
++ (BOOL)jumpHandleOpenURL:(NSURL *)url
+              withUrlData:(NSDictionary *)urldata
+             responseDelg:(id<ChieryJumpHandleResponsePrt>)responseDelg
+                 userInfo:(id)userInfo {
+    
+    if (!url) {
+        return NO;
+    }
+    
+    if (!url.scheme || !url.host) {
+        return NO;
+    }
+    
+    NSString *urlScheme = url.scheme;
+    NSString *urlHost = url.host;
+    
+    //读取plist
+    NSString *plistPath = [[NSBundle mainBundle] pathForResource:@"jumpMap" ofType:@"plist"];
+    if (!plistPath) {
+        return NO;
+    }
+    
+    NSMutableDictionary *dict = [[NSMutableDictionary alloc] initWithContentsOfFile:plistPath];
+    if (!dict) {
+        return NO;
+    }
+    
+    if (!dict[urlScheme]) {
+        return NO;
+    }
+    
+    NSDictionary *schemeData = dict[urlScheme];
+    if (!schemeData[urlHost]) {
+        return NO;
+    }
+    
+    NSString *responseClassString = schemeData[urlHost];
+    if (!responseClassString) {
+        return NO;
+    }
+    
+    if ([NSClassFromString(responseClassString) respondsToSelector:@selector(jumpHandleOpenURL:withUrlData:responseDelg:userInfo:)]
+        && [NSClassFromString(responseClassString) conformsToProtocol:@protocol(ChieryJumpHandlePrt)]) {
+        return [NSClassFromString(responseClassString) jumpHandleOpenURL:url withUrlData:urldata responseDelg:responseDelg userInfo:userInfo];
+    }
+    
+    return NO;
+}
+
++ (void)jumpHandleResponse:(__weak id<ChieryJumpHandleResponsePrt>)responseDelg
+              responseData:(NSDictionary *)responseData
+               withOpenURL:(NSURL *)url
+                   urlData:(NSDictionary *)urlData
+                  userInfo:(id)userInfo {
+    if ([responseDelg respondsToSelector:@selector(jumpHandleResponse:responseData:withOpenURL:urlData:userInfo:)]) {
+        [responseDelg jumpHandleResponseData:responseData withOpenURL:url urlData:urlData userInfo:userInfo];
+    }
+}
 
 @end
