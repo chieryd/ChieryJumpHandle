84c0556a344b8c0cb1e95e2e2c1033831271c76e
HanDong Wang <handongwang@HanDongdeMacBook-Pro.local>
2018-01-11 21:00:27 +0800

    更改文件结构

---
+++ ChieryJumpHandle/ChieryJumpHandle_a.h
@@ -0,0 +1,13 @@
+//
+//  ChieryJumpHandle_a.h
+//  ChieryJumpHandle_a
+//
+//  Created by HanDong Wang on 2018/1/11.
+//  Copyright © 2018年 com.chiery.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ChieryJumpHandle_a : NSObject
+
+@end
